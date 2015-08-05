e80bf382951bc6747033066b8e831e569b1267c3
unknown <lecruz@AG161.expoente.com.br>
2015-08-05 15:35:12 -0300

    Inserção de arquivos de áudio e imagens, limpeza de arquivo CSS, inserção de elementos na tela e teste básico de clique.
    Signed-off-by: unknown <lecruz@AG161.expoente.com.br>

--- www/js/index.js
+++ www/js/index.js
@@ -80,16 +80,9 @@ var app = {
             'OK'                         // buttonName
         );
         playAudio("http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3");
+        $("#opcoes a").click(function(event) {
+            event.preventDefault();
+            alert("Clique capturado!");
+        });
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
